a9e6e08b4105c7379b621eb63823ea4cd279f217
Alexandre Poirot <alexandre.poirot@pix4d.com>
2022-10-03 19:30:48 +0200

    Update version to 0.0.3

--- cpp_packaging/conanfile.py
+++ cpp_packaging/conanfile.py
@@ -7,7 +7,7 @@ import glob
 
 class qbjsDeserializerConan(ConanFile):
     name = "qbjs_deserializer"
-    lib_version = "0.0.2"
+    lib_version = "0.0.3"
     recipe_version = "0"
     version = "{}-{}".format(lib_version, recipe_version)
     generators = "cmake"
